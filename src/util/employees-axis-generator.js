aa2ff29f3b0c62445d1eecd03a7753b590121d77
Jmeas <jellyes2@gmail.com>
2015-11-04 15:54:06 -0500

    Remove unused employee axis generator.

--- src/util/employees-axis-generator.js
+++
@@ -1,7 +0,0 @@
-import _ from 'lodash';
-import crel from 'crel';
-
-export default function(employees) {
-  var children = _.map(employees, e => crel('div', e.name));
-  return crel('div', children);
-};
