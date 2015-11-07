1bb249665352d7c15c32431ef0925f8602ad32d7
Jmeas <jellyes2@gmail.com>
2015-11-07 00:48:26 -0500

    Add widths to the packing algorithm.

--- src/common/packing-util.js
+++ src/common/packing-util.js
@@ -5,49 +5,76 @@ import _ from 'lodash';
 // place a utilization "next" to another one, when there are overlaps.
 // Requires that the utilizations be sorted.
 export default function({rectangles, startProp, endProp}) {
-  // This is a list of the most recent rectangle in each position
-  var lastPositions = [];
+  // A "group" is a collection of rectangles that are linked by
+  // a series of overlaps. A group's width is the max number of
+  // columns in the group
+  var group = {
+    // Every rectangle within this group
+    members: [],
+    // This keeps track of the current rectangle within each column
+    columns: [],
+    end: undefined
+  };
 
-  _.each(rectangles, r => {
-    // If this is our first rectangle, then we place it in the
-    // first position.
-    if (!lastPositions.length) {
+  _.each(rectangles, (r, index) => {
+    // If we have nothing in our group, then this is the first rectangle
+    var isEmpty = !group.members.length;
+    // Is this utilization after the group?
+    var isAfter = r[startProp] > group.end;
+
+    // If we're first, or if we're beyond the group, then we
+    // make sure the group is clear, and make a new group.
+    if (isEmpty || isAfter) {
       r.position = 0;
-      r.columnSize = 1;
-      lastPositions[0] = r;
+      r.groupWidth = 1;
+      group.columns = [r];
+      group.members = [r];
+      group.end = r[endProp];
       return;
     }
 
-    // Find the first position that our rectangle comes after.
-    // That's the new position for this rectangle.
-    _.find(lastPositions, (p, i) => {
-      if (r[startProp] > p[endProp]) {
-        r.position = i;
-        // The width of the column is always one plus the position
-        r.columnSize = i + 1;
-        // Update the other things in this column
-        _.times(i, n => {
-          lastPosition[n].columnSize = i + 1;
-        });
+    // Add it to the current group
+    group.members.push(r);
+    // Update the end of the group if this extends its length
+    if (r[endProp] > group.end) {
+      group.end = r[endProp];
+    }
 
-        lastPositions[i] = r;
-        return true;
-      }
+    // Now that we've determined that a group already exists,
+    // and that this element belongs in the group, we must
+    // determine if it belongs in an existing column. If it
+    // does, then the total group size hasn't increased, so
+    // we only need to set the column and groupWidth for this
+    // rectangle.
+    var found = _.find(group.columns, (p, i) => {
+      // The rectangle fits into this column if it comes after the
+      // existing rectangle in this column.
+      var fits = r[startProp] > p[endProp];
+      // If it doesn't fit, then we can continue on our search.
+      if (!fits) { return; }
+      // The position is just this column's width
+      r.position = i;
+      // And it gets whatever width the group has
+      r.groupWidth = p.groupWidth;
+      // Make sure we set it in this column position
+      group.columns[i] = r;
+      return true;
     });
 
-    // If we found a position, then we exit
-    if (!_.isUndefined(r.position)) { return; }
+    // If we found an existing column to place the
+    // utilization in within the group, then we can exit
+    if (!_.isUndefined(found)) { return; }
 
-    // Otherwise, we set this in a new column, and update
-    // the width of everything before it.
-    var i = lastPositions.length;
+    // Otherwise, we need to make a new column and update
+    // the width of the entire group.
+    var i = group.columns.length;
     r.position = i;
-    r.columnSize = i + 1;
-    // Update the other things in this column
-    _.times(i, n => {
-      lastPositions[n].columnSize = i + 1;
+    // Update the whole group
+    _.each(group.members, m => {
+      m.groupWidth = i + 1;
     });
-    lastPositions[i] = r;
+    // Set this rectangle in this column in the group
+    group.columns[i] = r;
   });
 
   return rectangles;
--- test/unit/common/packing-util.js
+++ test/unit/common/packing-util.js
@@ -1,10 +1,17 @@
 import packingUtil from '../../../src/common/packing-util';
 
-describe('quantize', () => {
+describe('packingUtil', () => {
   it('should be a function', () => {
     expect(packingUtil).to.be.a('function');
   });
 
+  //
+  //  1
+  //  1
+  //
+  //  2
+  //  2
+  //
   describe('two rectangles that do not overlap with space between them', () => {
     var one, two, result, rectangles;
     beforeEach(() => {
@@ -14,8 +21,8 @@ describe('quantize', () => {
       };
 
       two = {
-        start: 5,
-        end: 10
+        start: 4,
+        end: 5
       };
 
       rectangles = [one, two];
@@ -38,6 +45,12 @@ describe('quantize', () => {
     });
   });
 
+  //
+  //  1
+  //  1
+  //  2
+  //  2
+  //
   describe('two rectangles that do not overlap that are bordering one another', () => {
     var one, two, result, rectangles;
     beforeEach(() => {
@@ -48,7 +61,7 @@ describe('quantize', () => {
 
       two = {
         start: 3,
-        end: 10
+        end: 4
       };
 
       rectangles = [one, two];
@@ -70,23 +83,29 @@ describe('quantize', () => {
       expect(result[1].position).to.equal(0);
     });
 
-    it('should give both of them a columnSize of 1', () => {
-      expect(result[0].columnSize).to.equal(1);
-      expect(result[1].columnSize).to.equal(1);
+    it('should give both of them a groupWidth of 1', () => {
+      expect(result[0].groupWidth).to.equal(1);
+      expect(result[1].groupWidth).to.equal(1);
     });
   });
 
+  //
+  //  1 2
+  //  1 2
+  //  1 2
+  //  1 2
+  //
   describe('two rectangles that are the same', () => {
     var one, two, result, rectangles;
     beforeEach(() => {
       one = {
         start: 1,
-        end: 5
+        end: 4
       };
 
       two = {
         start: 1,
-        end: 5
+        end: 4
       };
 
       rectangles = [one, two];
@@ -111,96 +130,21 @@ describe('quantize', () => {
       expect(result[1].position).to.equal(1);
     });
 
-    it('should give them both columnSize of 2', () => {
-      expect(result[0].columnSize).to.equal(2);
-      expect(result[1].columnSize).to.equal(2);
+    it('should give them both groupWidth of 2', () => {
+      expect(result[0].groupWidth).to.equal(2);
+      expect(result[1].groupWidth).to.equal(2);
     });
   });
 
+  //
+  //  1
+  //  1
+  //  1 2
+  //  1 2
+  //  1
+  //
   describe('two rectangles, where the first "contains" the second', () => {
     var one, two, result, rectangles;
-    beforeEach(() => {
-      one = {
-        start: 1,
-        end: 15
-      };
-
-      two = {
-        start: 5,
-        end: 6
-      };
-
-      rectangles = [one, two];
-
-      result = packingUtil({
-        rectangles,
-        startProp: 'start',
-        endProp: 'end'
-      });
-    });
-
-    it('should return the original array, with the same length', () => {
-      expect(result).to.have.length(2);
-      expect(result).to.have.equal(rectangles);
-    });
-
-    it('should place the first in position 0', () => {
-      expect(result[0].position).to.equal(0);
-    });
-
-    it('should place the second in position 1', () => {
-      expect(result[1].position).to.equal(1);
-    });
-
-    it('should give them both columnSize of 2', () => {
-      expect(result[0].columnSize).to.equal(2);
-      expect(result[1].columnSize).to.equal(2);
-    });
-  });
-
-  describe('two rectangles, where the second extends beyond the first', () => {
-    var one, two, result, rectangles;
-    beforeEach(() => {
-      one = {
-        start: 1,
-        end: 15
-      };
-
-      two = {
-        start: 5,
-        end: 20
-      };
-
-      rectangles = [one, two];
-
-      result = packingUtil({
-        rectangles,
-        startProp: 'start',
-        endProp: 'end'
-      });
-    });
-
-    it('should return the original array, with the same length', () => {
-      expect(result).to.have.length(2);
-      expect(result).to.have.equal(rectangles);
-    });
-
-    it('should place the first in position 0', () => {
-      expect(result[0].position).to.equal(0);
-    });
-
-    it('should place the second in position 1', () => {
-      expect(result[1].position).to.equal(1);
-    });
-
-    it('should give them both columnSize of 2', () => {
-      expect(result[0].columnSize).to.equal(2);
-      expect(result[1].columnSize).to.equal(2);
-    });
-  });
-
-  describe('three rectangles that can be packed tightly', () => {
-    var one, two, three, result, rectangles;
     beforeEach(() => {
       one = {
         start: 1,
@@ -209,12 +153,109 @@ describe('quantize', () => {
 
       two = {
         start: 3,
-        end: 7
+        end: 4
+      };
+
+      rectangles = [one, two];
+
+      result = packingUtil({
+        rectangles,
+        startProp: 'start',
+        endProp: 'end'
+      });
+    });
+
+    it('should return the original array, with the same length', () => {
+      expect(result).to.have.length(2);
+      expect(result).to.have.equal(rectangles);
+    });
+
+    it('should place the first in position 0', () => {
+      expect(result[0].position).to.equal(0);
+    });
+
+    it('should place the second in position 1', () => {
+      expect(result[1].position).to.equal(1);
+    });
+
+    it('should give them both groupWidth of 2', () => {
+      expect(result[0].groupWidth).to.equal(2);
+      expect(result[1].groupWidth).to.equal(2);
+    });
+  });
+
+  //
+  //  1
+  //  1 2
+  //  1 2
+  //    2
+  //    2
+  //
+  describe('two rectangles, where the second extends beyond the first', () => {
+    var one, two, result, rectangles;
+    beforeEach(() => {
+      one = {
+        start: 1,
+        end: 3
+      };
+
+      two = {
+        start: 2,
+        end: 5
+      };
+
+      rectangles = [one, two];
+
+      result = packingUtil({
+        rectangles,
+        startProp: 'start',
+        endProp: 'end'
+      });
+    });
+
+    it('should return the original array, with the same length', () => {
+      expect(result).to.have.length(2);
+      expect(result).to.have.equal(rectangles);
+    });
+
+    it('should place the first in position 0', () => {
+      expect(result[0].position).to.equal(0);
+    });
+
+    it('should place the second in position 1', () => {
+      expect(result[1].position).to.equal(1);
+    });
+
+    it('should give them both groupWidth of 2', () => {
+      expect(result[0].groupWidth).to.equal(2);
+      expect(result[1].groupWidth).to.equal(2);
+    });
+  });
+
+  //
+  //  1
+  //  1 2
+  //  1 2
+  //  3 2
+  //  3 2
+  //  3
+  //
+  describe('three rectangles that can be packed tightly', () => {
+    var one, two, three, result, rectangles;
+    beforeEach(() => {
+      one = {
+        start: 1,
+        end: 3
+      };
+
+      two = {
+        start: 2,
+        end: 5
       };
 
       three = {
-        start: 6,
-        end: 9
+        start: 4,
+        end: 6
       };
 
       rectangles = [one, two, three];
@@ -243,29 +284,38 @@ describe('quantize', () => {
       expect(result[2].position).to.equal(0);
     });
 
-    it('should give them all columnSize of 2', () => {
-      expect(result[0].columnSize).to.equal(2);
-      expect(result[1].columnSize).to.equal(2);
-      expect(result[2].columnSize).to.equal(2);
+    it('should give them all groupWidth of 2', () => {
+      expect(result[0].groupWidth).to.equal(2);
+      expect(result[1].groupWidth).to.equal(2);
+      expect(result[2].groupWidth).to.equal(2);
     });
   });
 
+  //
+  //  1
+  //  1
+  //
+  //  2
+  //  2 3
+  //  2 3
+  //    3
+  //
   describe('two overlaps, one loner', () => {
     var one, two, three, result, rectangles;
     beforeEach(() => {
       one = {
         start: 1,
-        end: 5
+        end: 2
       };
 
       two = {
-        start: 6,
-        end: 10
+        start: 4,
+        end: 6
       };
 
       three = {
-        start: 9,
-        end: 11
+        start: 5,
+        end: 7
       };
 
       rectangles = [one, two, three];
@@ -292,6 +342,213 @@ describe('quantize', () => {
 
     it('should place the third in position 1', () => {
       expect(result[2].position).to.equal(1);
+    });
+
+    it('should place the first length of 1', () => {
+      expect(result[0].groupWidth).to.equal(1);
+    });
+
+    it('should place the second to be of length 2', () => {
+      expect(result[1].groupWidth).to.equal(2);
+    });
+
+    it('should place the third to be of length 2', () => {
+      expect(result[2].groupWidth).to.equal(2);
+    });
+  });
+
+  //
+  //  1 2 3 4 5
+  //  1 2 3 4 5
+  //  1 2 3 4 5
+  //  1 2 3 4 5
+  //  1 2 3 4 5
+  //
+  describe('four overlaps', () => {
+    var one, two, three, four, rectangles, result;
+    beforeEach(() => {
+      one = {
+        start: 1,
+        end: 5
+      };
+
+      two = {
+        start: 1,
+        end: 5
+      };
+
+      three = {
+        start: 1,
+        end: 5
+      };
+
+      four = {
+        start: 1,
+        end: 5
+      };
+
+      rectangles = [one, two, three, four];
+
+      result = packingUtil({
+        rectangles,
+        startProp: 'start',
+        endProp: 'end'
+      });
+    });
+
+    it('should return the original array, with the same length', () => {
+      expect(result).to.have.length(4);
+      expect(result).to.have.equal(rectangles);
+    });
+
+    it('should place them all next to one another', () => {
+      expect(result[0].position).to.equal(0);
+      expect(result[1].position).to.equal(1);
+      expect(result[2].position).to.equal(2);
+      expect(result[3].position).to.equal(3);
+    });
+
+    it('should give them all length of 4', () => {
+      expect(result[0].groupWidth).to.equal(4);
+      expect(result[1].groupWidth).to.equal(4);
+      expect(result[2].groupWidth).to.equal(4);
+      expect(result[3].groupWidth).to.equal(4);
+    });
+  });
+
+  //
+  //  1
+  //  1 2 3
+  //  1 2
+  //  1 4
+  //  1
+  //
+  describe('a complex system', () => {
+    var one, two, three, four, five, rectangles, result;
+    beforeEach(() => {
+      one = {
+        start: 1,
+        end: 5
+      };
+
+      two = {
+        start: 2,
+        end: 3
+      };
+
+      three = {
+        start: 2,
+        end: 2
+      };
+
+      four = {
+        start: 4,
+        end: 4
+      };
+
+      rectangles = [one, two, three, four];
+
+      result = packingUtil({
+        rectangles,
+        startProp: 'start',
+        endProp: 'end'
+      });
+    });
+
+    it('should return the original array, with the same length', () => {
+      expect(result).to.have.length(4);
+      expect(result).to.have.equal(rectangles);
+    });
+
+    it('should get the positions correct', () => {
+      expect(result[0].position).to.equal(0);
+      expect(result[1].position).to.equal(1);
+      expect(result[2].position).to.equal(2);
+      expect(result[3].position).to.equal(1);
+    });
+
+    it('should get the groupWidths correct', () => {
+      expect(result[0].groupWidth).to.equal(3);
+      expect(result[1].groupWidth).to.equal(3);
+      expect(result[2].groupWidth).to.equal(3);
+      expect(result[3].groupWidth).to.equal(3);
+    });
+  });
+
+  //
+  //  1
+  //  1
+  //  1 2
+  //    2
+  //  3 2
+  //  3 4
+  //  3 4 5
+  //      5
+  //  6   5
+  //
+  describe('another complex system', () => {
+    var one, two, three, four, five, six, rectangles, result;
+    beforeEach(() => {
+      one = {
+        start: 1,
+        end: 3
+      };
+
+      two = {
+        start: 3,
+        end: 5
+      };
+
+      three = {
+        start: 5,
+        end: 7
+      };
+
+      four = {
+        start: 6,
+        end: 7
+      };
+
+      five = {
+        start: 7,
+        end: 9
+      };
+
+      six = {
+        start: 9,
+        end: 9
+      };
+
+      rectangles = [one, two, three, four, five, six];
+
+      result = packingUtil({
+        rectangles,
+        startProp: 'start',
+        endProp: 'end'
+      });
+    });
+
+    it('should return the original array, with the same length', () => {
+      expect(result).to.have.length(6);
+      expect(result).to.have.equal(rectangles);
+    });
+
+    it('should get the positions correct', () => {
+      expect(result[0].position).to.equal(0);
+      expect(result[1].position).to.equal(1);
+      expect(result[2].position).to.equal(0);
+      expect(result[3].position).to.equal(1);
+      expect(result[4].position).to.equal(2);
+      expect(result[5].position).to.equal(0);
+    });
+
+    it('should get the groupWidths correct', () => {
+      expect(result[0].groupWidth).to.equal(3);
+      expect(result[1].groupWidth).to.equal(3);
+      expect(result[2].groupWidth).to.equal(3);
+      expect(result[3].groupWidth).to.equal(3);
+      expect(result[4].groupWidth).to.equal(3);
+      expect(result[5].groupWidth).to.equal(3);
     });
   });
 });
