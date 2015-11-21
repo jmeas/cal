120ad71d2113f91ff64a1ed5275a38a01ff8bf4c
Jmeas <jellyes2@gmail.com>
2015-11-21 12:09:13 -0500

    Support covering in quantization algorithm

--- src/common/quantize.js
+++ src/common/quantize.js
@@ -1,4 +1,6 @@
 // Quantize a `val` by `quantum`
-export default function(val, quantum) {
-  return (val - (val % quantum)) / quantum;
+export default function(val, quantum, {cover = false} = {}) {
+  // The `cover` option determines if we're using a covering or a fitting algorithm
+  var mod = cover ? 1 : 0;
+  return (val - (val % quantum)) / quantum + mod;
 }
--- test/unit/common/quantize.js
+++ test/unit/common/quantize.js
@@ -5,15 +5,21 @@ describe('quantize', () => {
     expect(quantize).to.be.a('function');
   });
 
-  it('should quantize values that divide evenly correctly', function() {
+  it('should quantize values that divide evenly correctly', () => {
     expect(quantize(9, 3)).to.equal(3);
     expect(quantize(100, 2)).to.equal(50);
     expect(quantize(25, 5)).to.equal(5);
   });
 
-  it('should quantize values that do not divide evenly correctly', function() {
+  it('should quantize values that do not divide evenly correctly', () => {
     expect(quantize(9, 2)).to.equal(4);
     expect(quantize(12, 5)).to.equal(2);
     expect(quantize(77, 25)).to.equal(3);
+  });
+
+  it('should support a covering algorithm by passing cover:true', () => {
+    expect(quantize(9, 2, {cover: true})).to.equal(5);
+    expect(quantize(12, 5, {cover: true})).to.equal(3);
+    expect(quantize(77, 25, {cover: true})).to.equal(4);
   });
 });
