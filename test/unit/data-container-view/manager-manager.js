79d00111b7141cae661f0ba1003e03e1ca909b54
Jmeas <jellyes2@gmail.com>
2015-11-24 23:48:30 -0500

    Continued work on ManagerManager tests

--- test/unit/data-container-view/manager-manager.js
+++ test/unit/data-container-view/manager-manager.js
@@ -45,7 +45,7 @@ describe('ManagerManager', () => {
         });
 
         managerManager._managers.forEach(m => {
-          expect(m.clear).to.not.have.been.called;
+          expect(m.clear).to.have.not.been.called;
         });
       });
     });
@@ -67,9 +67,9 @@ describe('ManagerManager', () => {
         });
 
         it('should not clear the other managers', () => {
-          expect(managerManager._managers[0].clear).to.not.have.been.called;
-          expect(managerManager._managers[1].clear).to.not.have.been.called;
-          expect(managerManager._managers[5].clear).to.not.have.been.called;
+          expect(managerManager._managers[0].clear).to.have.not.been.called;
+          expect(managerManager._managers[1].clear).to.have.not.been.called;
+          expect(managerManager._managers[5].clear).to.have.not.been.called;
         });
       });
 
@@ -87,11 +87,11 @@ describe('ManagerManager', () => {
         });
 
         it('should not clear the other managers', () => {
-          expect(managerManager._managers[0].clear).to.not.have.been.called;
-          expect(managerManager._managers[1].clear).to.not.have.been.called;
-          expect(managerManager._managers[2].clear).to.not.have.been.called;
-          expect(managerManager._managers[3].clear).to.not.have.been.called;
-          expect(managerManager._managers[4].clear).to.not.have.been.called;
+          expect(managerManager._managers[0].clear).to.have.not.been.called;
+          expect(managerManager._managers[1].clear).to.have.not.been.called;
+          expect(managerManager._managers[2].clear).to.have.not.been.called;
+          expect(managerManager._managers[3].clear).to.have.not.been.called;
+          expect(managerManager._managers[4].clear).to.have.not.been.called;
         });
       });
     });
@@ -112,10 +112,10 @@ describe('ManagerManager', () => {
         });
 
         it('should not clear the other managers', () => {
-          expect(managerManager._managers[0].clear).to.not.have.been.called;
-          expect(managerManager._managers[1].clear).to.not.have.been.called;
-          expect(managerManager._managers[4].clear).to.not.have.been.called;
-          expect(managerManager._managers[5].clear).to.not.have.been.called;
+          expect(managerManager._managers[0].clear).to.have.not.been.called;
+          expect(managerManager._managers[1].clear).to.have.not.been.called;
+          expect(managerManager._managers[4].clear).to.have.not.been.called;
+          expect(managerManager._managers[5].clear).to.have.not.been.called;
         });
       });
 
@@ -133,11 +133,11 @@ describe('ManagerManager', () => {
         });
 
         it('should not clear the other managers', () => {
-          expect(managerManager._managers[1].clear).to.not.have.been.called;
-          expect(managerManager._managers[2].clear).to.not.have.been.called;
-          expect(managerManager._managers[3].clear).to.not.have.been.called;
-          expect(managerManager._managers[4].clear).to.not.have.been.called;
-          expect(managerManager._managers[5].clear).to.not.have.been.called;
+          expect(managerManager._managers[1].clear).to.have.not.been.called;
+          expect(managerManager._managers[2].clear).to.have.not.been.called;
+          expect(managerManager._managers[3].clear).to.have.not.been.called;
+          expect(managerManager._managers[4].clear).to.have.not.been.called;
+          expect(managerManager._managers[5].clear).to.have.not.been.called;
         });
       });
     });
@@ -185,10 +185,10 @@ describe('ManagerManager', () => {
       });
 
       it('should not call `render` on the other managers', () => {
-        expect(managerManager._managers[0].render).to.not.have.been.called;
-        expect(managerManager._managers[1].render).to.not.have.been.called;
-        expect(managerManager._managers[2].render).to.not.have.been.called;
-        expect(managerManager._managers[5].render).to.not.have.been.called;
+        expect(managerManager._managers[0].render).to.have.not.been.called;
+        expect(managerManager._managers[1].render).to.have.not.been.called;
+        expect(managerManager._managers[2].render).to.have.not.been.called;
+        expect(managerManager._managers[5].render).to.have.not.been.called;
       });
     });
 
@@ -210,10 +210,149 @@ describe('ManagerManager', () => {
       });
 
       it('should not call `render` on the other managers', () => {
-        expect(managerManager._managers[0].render).to.not.have.been.called;
-        expect(managerManager._managers[1].render).to.not.have.been.called;
-        expect(managerManager._managers[2].render).to.not.have.been.called;
-        expect(managerManager._managers[3].render).to.not.have.been.called;
+        expect(managerManager._managers[0].render).to.have.not.been.called;
+        expect(managerManager._managers[1].render).to.have.not.been.called;
+        expect(managerManager._managers[2].render).to.have.not.been.called;
+        expect(managerManager._managers[3].render).to.have.not.been.called;
+      });
+    });
+  });
+
+  describe('update', () => {
+    beforeEach(() => {
+      managerManager = new ManagerManager();
+      managerManager._managers = _.map(Array(6), (v, i) => {
+        return {
+          render: stub()
+        };
+      });
+
+      stub(managerManager, '_broadcastUpdate');
+      stub(managerManager, '_broadcastDelete');
+    });
+
+    describe('when the indices are unchanged', () => {
+      it('should not call broadcastUpdate nor broadcastDelete', () => {
+        managerManager._firstXIndex = 5;
+        managerManager._lastXIndex = 6;
+        managerManager._firstYIndex = 2;
+        managerManager._lastYIndex = 3;
+
+        managerManager.update({
+          firstXIndex: 5,
+          lastXIndex: 6,
+          firstYIndex: 2,
+          lastYIndex: 3
+        });
+
+        expect(managerManager._broadcastUpdate).to.have.not.been.called;
+        expect(managerManager._broadcastDelete).to.have.not.been.called;
+      });
+    });
+
+    describe('when there are no indices set', () => {
+      beforeEach(() => {
+        managerManager.update({
+          firstXIndex: 5,
+          lastXIndex: 6,
+          firstYIndex: 2,
+          lastYIndex: 3,
+          xDirection: -1,
+          yDirection: -1,
+        });
+      });
+
+      it('should not call broadcastDelete', () => {
+        expect(managerManager._broadcastDelete).to.have.not.been.called;
+      });
+
+      it('should call broadcastUpdate with the correct args', () => {
+        expect(managerManager._broadcastUpdate).to.have.been.calledOnce;
+        expect(managerManager._broadcastUpdate).to.have.been.calledWithExactly({
+          clear: true,
+          firstXIndex: 5,
+          lastXIndex: 6,
+          xDirection: 1,
+          yDirection: -1,
+          firstYIndex: 2,
+          lastYIndex: 3
+        });
+      });
+    });
+
+    describe('when there indices set, and it moves 1 direction forward', () => {
+      beforeEach(() => {
+        managerManager._firstXIndex = 4;
+        managerManager._lastXIndex = 5;
+
+        managerManager.update({
+          firstXIndex: 5,
+          lastXIndex: 6,
+          firstYIndex: 2,
+          lastYIndex: 3,
+          xDirection: 1,
+          yDirection: -1,
+        });
+      });
+
+      it('should call broadcastDelete with the correct args', () => {
+        expect(managerManager._broadcastDelete).to.have.been.calledOnce;
+        expect(managerManager._broadcastDelete).to.have.been.calledWithExactly({
+          direction: 1,
+          removeDelta: 1,
+          referenceIndex: 4
+        });
+      });
+
+      it('should call broadcastUpdate with the correct args', () => {
+        expect(managerManager._broadcastUpdate).to.have.been.calledOnce;
+        expect(managerManager._broadcastUpdate).to.have.been.calledWithExactly({
+          clear: false,
+          firstXIndex: 5,
+          lastXIndex: 6,
+          xDirection: 1,
+          yDirection: -1,
+          firstYIndex: 2,
+          lastYIndex: 3
+        });
+      });
+    });
+
+    describe('when there indices set, and it moves 1 direction backward', () => {
+      beforeEach(() => {
+        managerManager._firstXIndex = 4;
+        managerManager._lastXIndex = 5;
+
+        managerManager.update({
+          firstXIndex: 3,
+          lastXIndex: 4,
+          firstYIndex: 2,
+          lastYIndex: 3,
+          xDirection: -1,
+          yDirection: -1,
+        });
+      });
+
+      it('should call broadcastDelete with the correct args', () => {
+        expect(managerManager._broadcastDelete).to.have.been.calledOnce;
+        expect(managerManager._broadcastDelete).to.have.been.calledWithExactly({
+          direction: -1,
+          removeDelta: 1,
+          referenceIndex: 5
+        });
+      });
+
+      it('should call broadcastUpdate with the correct args', () => {
+        expect(managerManager._broadcastUpdate).to.have.been.calledOnce;
+        expect(managerManager._broadcastUpdate).to.have.been.calledWithExactly({
+          clear: false,
+          firstXIndex: 3,
+          lastXIndex: 4,
+          xDirection: -1,
+          yDirection: -1,
+          firstYIndex: 2,
+          lastYIndex: 3
+        });
       });
     });
   });
